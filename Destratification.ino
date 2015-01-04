3521fb14143823be30e510215cf5e46c7dd9ad4d
adwalton <walton.andrew1@gmail.com>
2015-01-04 09:09:32 +0000

    Increased length of energy averaging to 4 mins. Reduced length of main loop by reducing / removing delays and shortening LED flash warning for Low Temp

--- Destratification.ino
+++ Destratification.ino
@@ -29,9 +29,8 @@
   16/12/2014 - Adjusted energy array and sample time to 40 samples 15S apart. Also increased 'temp steady' threshold from 0.01 to 0.03. Added backlight control for LCD - turn it off 
                when energy level is static (and unless cylinder is full
   18/12/2014 - Adjusted energy samples to 60 at 10S intervals (total 10 mins). Reduced "P" from 30 to 25 to slow down boiler heating and reduce over shoot.
-  03-01-2015 - Adjusted energy array to 6 at 10S intervals (total 1 min) 
+  03-01-2015 - Adjusted energy array to 24 at 10S intervals (total 4 mins). Also removed 2S delay from end of main loop
  */
-
 // include the library code:
 #include <PID_v1.h>
 #include <Wire.h>
@@ -69,7 +68,7 @@ double bottomTemp;
 double relaySetPoint = 42; // middleTemp value at which relay will be energised and prevent further gas heating of the water
 float energy; // Variable to hold calculated energy above 15C that gives an indication of the total heat in the cylinder
 float maxEnergy = 8.2; // total capacity of the cylinder in kWh - used to halt destrat activity (Was 7.8)
-const unsigned nRecentEnergies = 6; //Number of recent energy values to store. MUST BE EVEN.
+const unsigned nRecentEnergies = 24; //Number of recent energy values to store. MUST BE EVEN.
 float recentEnergies[nRecentEnergies]; // Create array to store energy readings
 unsigned recentEnergiesIndex = 0; // initialise pointer to energy array
 unsigned recentEnergiesInterval = 10000; // time between successive energy readings (ms)
@@ -201,50 +200,21 @@ void loop() {
    //
    // FLASH ALL LEDs to Indicate LOW TEMP
    digitalWrite(offLEDPin,HIGH);
-   digitalWrite(pumpLEDPin,LOW);
-   digitalWrite(fullLEDPin,LOW);
-   delay(100);
-   digitalWrite(offLEDPin,LOW);
    digitalWrite(pumpLEDPin,HIGH);
-   digitalWrite(fullLEDPin,LOW);
-   delay(100);
-   digitalWrite(offLEDPin,LOW);
-   digitalWrite(pumpLEDPin,LOW);
    digitalWrite(fullLEDPin,HIGH);
    delay(100);
    digitalWrite(offLEDPin,LOW);
-   digitalWrite(pumpLEDPin,HIGH);
+   digitalWrite(pumpLEDPin,LOW);
    digitalWrite(fullLEDPin,LOW);
    delay(100);
    digitalWrite(offLEDPin,HIGH);
-   digitalWrite(pumpLEDPin,LOW);
-   digitalWrite(fullLEDPin,LOW);
-   delay(100);
-   digitalWrite(offLEDPin,LOW);
    digitalWrite(pumpLEDPin,HIGH);
-   digitalWrite(fullLEDPin,LOW);
-   delay(100);
-   digitalWrite(offLEDPin,LOW);
-   digitalWrite(pumpLEDPin,LOW);
    digitalWrite(fullLEDPin,HIGH);
    delay(100);
    digitalWrite(offLEDPin,LOW);
-   digitalWrite(pumpLEDPin,HIGH);
-   digitalWrite(fullLEDPin,LOW);
-   delay(100);
-   digitalWrite(offLEDPin,HIGH);
    digitalWrite(pumpLEDPin,LOW);
    digitalWrite(fullLEDPin,LOW);
-   delay(100);
-   digitalWrite(offLEDPin,LOW);
-   digitalWrite(pumpLEDPin,LOW);
-   digitalWrite(fullLEDPin,LOW);
-   delay(100);
-   //lcd.noBacklight();
-   //
-   //digitalWrite(offLEDPin,LOW); //set Amber/Red LED OFF - to generate flash effect on this LED while capacity is below lowAlarm threshhold
-   //
-   delay(2000);
+   delay(1000);
    lcd.clear();
   }
   else // do nothing
@@ -413,7 +383,7 @@ void loop() {
     analogWrite(pumpPin,pumpSpeed);
     }
 //
-  delay(2000);
+  //delay(2000);
 } // end of main Loop
 //
   // Function to ConvertADC Values to Degrees C
